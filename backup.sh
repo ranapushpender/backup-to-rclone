7a6fe83c0ed7f90996fe62ad68a9248255130726
Pushpender Rana <pushpender661@gmail.com>
2020-05-22 20:57:50 +0530

    Added completed message

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 export DATE=`date +"%d-%m-%Y_%H-%M-%S"`
+export GZIP=-9
 tar -cvzf /root/backups/backup-$NAME-$DATE.tar.gz /home
 rclone copy /root/backups/* $REMOTE:$BUCKET/$DIRECTORY
 rm -rf /root/backups/*
+echo "Backup Completed"
