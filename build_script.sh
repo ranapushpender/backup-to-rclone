5e27d5a1d774532dbf3aa671ebdba4272ad2926c
Pushpender Rana <pushpender661@gmail.com>
2020-10-14 21:50:07 +0530

    Add cd

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,6 @@
 apk update && apk add git
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
-docker build -t ranapushpender/backup-server:latest -t ranapushpender/backup-server:$TAG_NAME -f Dockerfile ./backup
+cd ./backup
+docker build -t ranapushpender/backup-server:latest -t ranapushpender/backup-server:$TAG_NAME -f Dockerfile backup
 docker push ranapushpender/backup-server:latest
 docker push ranapushpender/backup-server:$TAG_NAME
